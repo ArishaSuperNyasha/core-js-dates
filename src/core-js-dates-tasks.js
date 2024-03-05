3a25f1e778fe08a318dec5c8d4c6e0549eac4758
ArishaSuperNyasha <evangelionyouthresort@gmail.com>
2024-03-05 21:31:26 +0300

    feat: resolve first 3 problems

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -17,8 +17,8 @@
  * '01 Jan 1970 00:00:00 UTC' => 0
  * '04 Dec 1995 00:12:00 UTC' => 818035920000
  */
-function dateToTimestamp(/* date */) {
-  throw new Error('Not implemented');
+function dateToTimestamp(date) {
+  return Date.parse(date);
 }
 
 /**
@@ -31,8 +31,10 @@ function dateToTimestamp(/* date */) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  return [date.getHours(), date.getMinutes(), date.getSeconds()]
+    .map((i) => `${i}`.padStart(2, '0'))
+    .join(':');
 }
 
 /**
@@ -46,8 +48,17 @@ function getTime(/* date */) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const days = {
+    0: 'Sunday',
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday',
+  };
+  return days[new Date(date).getDay()];
 }
 
 /**
